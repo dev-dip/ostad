a6f3fe952ec8f48ad4f068c1d67cb2df0903f76b
dev-dip <diptosa.647@gmail.com>
2024-09-07 23:49:22 +0600

    final

---
+++ lib/data/products.dart
@@ -0,0 +1,49 @@
+import 'package:ostad/product_model.dart';
+
+final List<ProductModel> products = [
+  ProductModel(
+    productID: "01",
+    productImgUrl: "https://blacknavybd.com/wp-content/uploads/2023/05/Black-oversized-tshirt-3.jpg",
+    productTitle: "Pullover",
+    availableColors: [
+      "Black",
+      "Grey",
+    ],
+    availableSizes: [
+      "M",
+      "L",
+      "XL",
+    ],
+    price: 51,
+  ),
+  ProductModel(
+    productID: "02",
+    productImgUrl: "https://static-01.daraz.com.bd/p/cdc2bbe0d9345131330f7fc105f76d0c.jpg",
+    productTitle: "T-Shirt",
+    availableColors: [
+      "Black",
+      "Grey",
+    ],
+    availableSizes: [
+      "M",
+      "L",
+      "XL",
+    ],
+    price: 30,
+  ),
+  ProductModel(
+    productID: "03",
+    productImgUrl: "https://ih1.redbubble.net/image.1494281174.1153/ssrco,slim_fit_t_shirt,mens,101010:01c5ca27c6,front,square_product,600x600.u1.jpg",
+    productTitle: "Sport Dress",
+    availableColors: [
+      "Black",
+      "Grey",
+    ],
+    availableSizes: [
+      "M",
+      "L",
+      "XL",
+    ],
+    price: 43,
+  ),
+];
